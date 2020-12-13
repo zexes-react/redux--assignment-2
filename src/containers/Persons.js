cfe46cfd74cdef4a69094495387c7ecdad224d65
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-12-13 15:47:34 +0100

    yea first commit

---
+++ src/containers/Persons.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions';
+
+class Persons extends Component {
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.prs.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onDeletePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        prs: state.persons // persons here must be same as in the personReducer ***
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPerson: () => dispatch({type: actionTypes.ADD}),
+        onDeletePerson: (id) => dispatch({type: actionTypes.DELETE, personId: id})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ src/store/personReducer.js
@@ -0,0 +1,32 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    persons: []  // must be same name used in Person class check comment *** in Person.js
+}
+
+const personReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD:
+
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+
+            return {
+                ...state,
+                persons: state.persons.concat(newPerson)
+
+            }
+        case actionTypes.DELETE:
+            return {
+                ...state,
+                persons: state.persons.filter(person => person.id !== action.personId)
+            }
+        default:
+            return state;
+    }
+}
+
+export default personReducer;
